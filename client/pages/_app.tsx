79475f9984f13d2c94307901fe92758952c565c7
baekcCode <baekcode@gmail.com>
2022-01-18 22:24:24 +0900

    feature(header): 임시 헤더 구현

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
+import Header from '../components/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
